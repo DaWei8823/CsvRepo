17c8d5e6045f5893c928333b680dc9aa6edd3bf3
David Ullmann <DavidL.Ullmann@gmail.com>
2019-11-16 17:15:54 -0500

    finished navigation property implementation

--- CsvRepo/CsvRepo.cs
+++ CsvRepo/CsvRepo.cs
@@ -27,23 +27,23 @@ namespace CsvRepo
         }
 
 
-        public IEnumerable<TItems> GetAll<TItems>()
+        public IEnumerable<TItems> GetAll<TItems>() where TItems : class
         {
-            var path = GetFileName<TItems>();
-            var instantiate = GetInstantiationFunc<TItems>();
+            var path = GetFileName(typeof(TItems));
+            var instantiate = GetInstantiationFunc(typeof(TItems));
 
             using (var file = _fileProvider.GetFile(path))
             {
                 string line;
                 file.ReadLine(); // advance reader past the header line
                 while ((line = file.ReadLine()) != null)
-                    yield return instantiate(Split(line));
+                    yield return instantiate(Split(line)) as TItems;
             }
         }
 
         public TItem Get<TItem, TKey>(TKey key)
         {
-            throw new NotImplementedException();
+            
         }
 
 
@@ -79,9 +79,9 @@ namespace CsvRepo
         }
 
         private object GetInternal(Type type, Func<object,bool> pred)
-        {
-            throw new NotImplementedException();
-        }
+            => Get
+            
+        
 
         private string GetFileName<TItem>()
             => GetFileName(typeof(TItem));
@@ -92,70 +92,78 @@ namespace CsvRepo
 
         //ToDo: What if columns are not in order that properties are defined in the class?
         //TODO: What if primary key has different name
-        private static Func<string[], T> GetInstantiationFunc<T>()
-        {
+        private Func<string[], object> GetInstantiationFunc(Type objectType)
+        {            
+            var propTypeAndInstantiators = objectType.GetProperties()
+                .Select((property, index) =>
+                    new
+                    {
+                        instantiator = PropertyInstantiatorMapping.ContainsKey(property.GetType())
+                                ? PropertyInstantiatorMapping[property.GetType()].Curry(index)
+                                : GetNavigationPropertyInstantiator(objectType, property.GetType()),
+                        property
+                    }
+                ).ToList();
 
-            var objType = typeof(T);
-                       
-            var propertyInstantiators = objType.GetProperties()
-                .Select(p => p.GetType())
-                .Select((propType, index) => PropertyInstantiatorMapping.ContainsKey(propType) 
-                    ? PropertyInstantiatorMapping[propType].Curry(index) 
-                    : GetNavigationPropertyInstantiator(objType, propType));           
-
+            return cells =>
+            {
+                var instance = Activator.CreateInstance<T>();
+                propTypeAndInstantiators.ForEach(pti => pti.property.SetValue(instance, pti.instantiator(cells)));
+                return instance;
+            };
         }
 
         private Func<string[], object> GetNavigationPropertyInstantiator(Type objectType, Type propertyType)
+            => typeof(IEnumerable).IsAssignableFrom(propertyType) && propertyType.IsGenericType 
+                ? MakeParentToChildInstantiationFunc(objectType, propertyType.GetGenericArguments()[0])
+                : MakeChildToParentInstantiationFunc(propertyType, objectType);               
+
+        private Func<string[], object> MakeParentToChildInstantiationFunc(Type parentType, Type childType)
         {
-            if(typeof(IEnumerable).IsAssignableFrom(propertyType) && propertyType.IsGenericType)
+            var primaryKeyFieldName = GetPrimaryKeyFieldName(parentType);
+
+            var primaryKeyIndex = GetPropertyIndex(parentType, primaryKeyFieldName);
+
+            if (primaryKeyIndex == null)
+                throw new ArgumentException($"Cannot find primary key {primaryKeyFieldName} in type {parentType.Name}");
+
+            var foreignKeyInChildTypeIndex = GetPropertyIndex(childType, primaryKeyFieldName);
+
+            if (foreignKeyInChildTypeIndex == null)
+                throw new ArgumentException($"Cannot find foreign key reference {primaryKeyFieldName} to {parentType.Name} in child type {childType.Name}");
+
+            return cells =>
             {
-                var childType = propertyType.GetGenericArguments()[0];
-                var primaryKeyFieldName = GetPrimaryKeyFieldName(objectType);
+                Func<object, bool> predicate = obj => string.Equals(
+                    obj.GetType().GetProperties()[foreignKeyInChildTypeIndex.Value].GetValue(obj).ToString(),
+                    cells[primaryKeyIndex.Value]);
 
-                var primaryKeyIndex = GetPropertyIndex(objectType, primaryKeyFieldName);
-
-                if (primaryKeyIndex == null)
-                    throw new ArgumentException($"Cannot find primary key {primaryKeyFieldName} in type {objectType.Name}");
-
-                var foreignKeyInChildTypeIndex = GetPropertyIndex(childType, primaryKeyFieldName);
-
-                if(foreignKeyInChildTypeIndex == null)
-                    throw new ArgumentException($"Cannot find foreign key reference {primaryKeyFieldName} to {objectType.Name} in child type {childType.Name}");
-
-                return cells =>
-                {
-                    Func<object, bool> predicate = obj => string.Equals(
-                        obj.GetType().GetProperties()[foreignKeyInChildTypeIndex.Value].GetValue(obj).ToString(),
-                        cells[primaryKeyIndex.Value]);
-
-                    return GetInternal(childType, predicate);
-                };
-            }
-
-            var foreignKeyIndex = GetPropertyIndex(objectType, GetPrimaryKeyFieldName(propertyType));              
-            
-            if (foreignKeyIndex != null)
-                return MakeChildToParentInstantiationFunc(propertyType, foreignKeyIndex.Value);
-                       
-
+                return GetInternal(childType, predicate);
+            };
         }
 
+        private Func<string[], object> MakeChildToParentInstantiationFunc(Type parentType, Type childType)
+        {
+            var foreingKey = GetPrimaryKeyFieldName(parentType);
 
-        private Func<string[], object> MakeChildToParentInstantiationFunc(Type parentType, int foreignKeyIndex)
-            => line => GetInternal(parentType, line[foreignKeyIndex]);
+            var foreignKeyIndex = GetPropertyIndex(childType, foreingKey);
+
+            if (foreignKeyIndex == null)
+                throw new ArgumentException($"Cannot find foreign key {foreingKey} reference to {parentType.Name} on type {childType.Name}");
+
+            return cells => GetInternal(parentType, cells[foreignKeyIndex.Value]);
+        }              
 
         private static int? GetPropertyIndex(Type type, string propertyName)
             => type.GetProperties()
                 .Select((p, i) => new { property = p, index = i })
                 .SingleOrDefault(a => a.property.Name == propertyName)
-            ?.index;               
+            ?.index;   
 
         private static string GetPrimaryKeyFieldName(Type type)
             => $"{type.Name}Id";
 
-
-
-
+        //ToDo: Fill Dictionary!!
         private static readonly IDictionary<Type, Func<int, string[], object>> PropertyInstantiatorMapping
             = new Dictionary<Type, Func<int, string[], object>>
         {
